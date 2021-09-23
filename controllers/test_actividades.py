63fc932e709a9b95e59d1b5549a865f7b65f3f62
henry de la cruz orellana <hendelacruz@gmail.com>
2021-09-23 18:55:47 -0500

    chore: creacion de proyecto flask y primera ruta de get actividades

---
+++ controllers/test_actividades.py
@@ -0,0 +1,27 @@
+from controllers.actividades import ActividadesController
+from app import app
+from unittest import TestCase
+
+class TestActividadesController(TestCase):
+  def setUp(self):
+        '''Es el metodo que servira para configurar mis tests de esta clase'''
+        self.app = app.test_client()
+
+  
+  def test_get_actividades(self):
+        respuesta = self.app.get('/actividades')
+
+        self.assertEqual(respuesta.json, dict(message=None, content=[{
+            "actividadId": 1,
+            "actividadNombre": "Ir a la playa"
+        },
+            {
+            "actividadId": 2,
+                "actividadNombre": "Cocinar"
+        },
+            {
+            "actividadId": 3,
+                "actividadNombre": "Ir al cumpleaños de mi abuela"
+        }]))
+
+        self.assertEqual(respuesta.status_code, 201)
---
+++ prueba.py
@@ -0,0 +1,7 @@
+def sumatoria(x, y):
+    return x + y
+
+
+resultado = sumatoria(5, 2)
+if resultado == 7:
+    print('funciono')
