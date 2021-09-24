a8bcba88b94046e80d9b46e4e51cb6d1c4c55f51
Henry De la cruz <hendelacruz@gmail.com>
2021-09-23 21:13:12 -0500

    chore: agregue endpoint para la creacion de actividades

--- controllers/actividades.py
+++ controllers/actividades.py
@@ -1,5 +1,14 @@
 from flask_restful import Resource, reqparse
 
+serializador = reqparse.RequestParser()
+serializador.add_argument(
+    'actividadNombre',
+    required=True,
+    location='json',
+    help='Falta la actividadNombre',
+    type=str
+)
+
 class ActividadesController(Resource):
     def get(self):
         return {
@@ -16,4 +25,17 @@ class ActividadesController(Resource):
                 "actividadId": 3,
                 "actividadNombre": "Ir al cumpleaños de mi abuela"
             }]
+        }, 201
+
+    def post(self):
+        data: dict = serializador.parse_args()
+        # logica para registrar la actividad en la bd
+        # ...
+        actividadCreada = {
+            "actividadId": 50,
+            "actividadNombre": data.get('actividadNombre')
+        }
+        return {
+            "message": "actividad creada exitosamente",
+            "content": actividadCreada
         }, 201
